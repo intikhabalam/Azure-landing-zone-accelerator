6a42391ad8a778d7a3aca79049d07aaf198316aa
intikhab alam <intikhabalam@gmail.com>
2024-04-04 18:59:53 +1300

    adding the example.

--- Platform landing zone accelerator/delete script/delete alz.ps1
+++ Platform landing zone accelerator/delete script/delete alz.ps1
@@ -1,13 +1,19 @@
 # Set the scope name, which represents the management group
 scopeName="YOUR_SCOPE_NAME_HERE"
 
+#for example:  $scopeName="GAC2024-platform"
+
 # Retrieve the list of policy assignments under the specified scope
-output=$(az policy assignment list --scope "/providers/Microsoft.Management/managementGroups/$scopeName" --query '[].name' -o tsv)
+$output = az policy assignment list --scope "/providers/Microsoft.Management/managementGroups/$scopeName" --query '[].name' -o tsv
 
 # Convert the output into an array of policy names
-policyNames=($output -split "`n")
+$policyNames = $output -split "`n"
+
+# check the list of policies
+$policyNames 
 
 # Iterate over each policy name and delete the policy assignment
-for policyName in "${policyNames[@]}"; do
-    az policy assignment delete --name "$policyName"
-done
+foreach ($policyName in $policyNames) {
+    az policy assignment delete --name $policyName
+}
+
